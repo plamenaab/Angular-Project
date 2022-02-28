a741a2db045714874523e19074552c9cf5bb3077
Plamena Baksheva <p.baksheva@blubito.com>
2022-02-28 17:39:13 +0200

    candidate list save function in the same component

--- src/app/partei/candidate-list/candidate-list.component.ts
+++ src/app/partei/candidate-list/candidate-list.component.ts
@@ -1,15 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-candidate-list',
   templateUrl: './candidate-list.component.html',
   styleUrls: ['./candidate-list.component.css']
 })
-export class CandidateListComponent implements OnInit {
+export class CandidateListComponent  {
   @Input() candidates!:string[];
+  @ViewChild('newCandidateListInput') newCandidateListInput!:ElementRef;
   constructor() { }
 
-  ngOnInit(): void {
+  // onCandidateAdd(newCandidateListInput: HTMLInputElement) {
+  //   candidate: this.newCandidateListInput.nativeElement.value;
+  //   newCandidateListInput.value="";
+	// }
+
+  onCandidateAdd(candidate: string) {
+    this.candidates.push(candidate)
   }
 
 }
